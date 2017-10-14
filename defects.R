21ebdcc73d1cec6d54186fc150baf51b92351e08
longhowlam <longhowlam@gmail.com>
2017-10-14 12:36:48 +0100

    CC

--- defects.R
+++ defects.R
@@ -7,10 +7,19 @@ library(ggplot2)
 ## Import open car data from RDW 
 defects = read_csv("Open_Data_RDW__Geconstateerde_Gebreken.csv")
 defects_descr = read_csv("Open_Data_RDW__Gebreken.csv")
-cars_basisdata = read_csv(
-  "Personenauto_basisdata.csv",
-  col_types = cols(`Datum eerste toelating` = col_date(format = "%d/%m/%Y"))
-)
+
+#cars_basisdata = read_csv(
+#  "Personenauto_basisdata.csv",
+#  col_types = cols(`Datum eerste toelating` = col_date(format = "%d/%m/%Y"))
+#)
+
+### split in three because then it fits on GIT
+cars_basisdata1 = readRDS("cars_basisdata1.RDs")
+cars_basisdata2 = readRDS("cars_basisdata2.RDs")
+cars_basisdata3 = readRDS("cars_basisdata3.RDs")
+
+cars_basisdata = bind_rows(cars_basisdata3, cars_basisdata2, cars_basisdata1)
+
 
 ## determin age of car
 cars_basisdata = cars_basisdata %>%
